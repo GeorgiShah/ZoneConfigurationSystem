ecd5b40ef98345175496de33bee1fee61e7ff636
georgijr <shahnazaryan.georgi@gmail.com>
2022-10-08 16:02:29 +0400

    Creating two separate modules. One is separate runnable app for admins. The other is common module which will be used both in admin module and in future user module

--- admin/src/main/java/am/system/AdminApplication.java
+++ admin/src/main/java/am/system/AdminApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SystemApplication {
+public class AdminApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SystemApplication.class, args);
+        SpringApplication.run(AdminApplication.class, args);
     }
 }
