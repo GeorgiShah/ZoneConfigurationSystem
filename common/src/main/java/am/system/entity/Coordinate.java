08fa8ef3d325ab547a22f1e4f854dbc297d562c0
georgijr <shahnazaryan.georgi@gmail.com>
2022-10-09 13:03:15 +0400

    Changing constructor to Lombok annotation

--- common/src/main/java/am/system/entity/Coordinate.java
+++ common/src/main/java/am/system/entity/Coordinate.java
@@ -10,18 +10,13 @@ import java.util.Objects;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class Coordinate {
     private double x;
 
     private double y;
 
     private double z;
-
-    public Coordinate(double x, double y, double z) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
-    }
 
     @Override
     public boolean equals(Object o) {
